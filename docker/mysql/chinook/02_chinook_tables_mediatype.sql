3a5384e4f5b2fd6e3d8454b4ac6c37faa1044fe1
fgonzalezc <43912606+fgonzalezc@users.noreply.github.com>
2022-02-03 13:53:33 +0100

    Contenedor Docker para la base de datos

---
+++ docker/mysql/chinook/02_chinook_tables_mediatype.sql
@@ -0,0 +1,3 @@
+LOCK TABLES `MediaType` WRITE;
+INSERT INTO `MediaType` VALUES (1,'MPEG audio file'),(2,'Protected AAC audio file'),(3,'Protected MPEG-4 video file'),(4,'Purchased AAC audio file'),(5,'AAC audio file');
+UNLOCK TABLES;
